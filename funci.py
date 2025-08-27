840f70f85be9b4e9e546c7b6fa1103a04995f195
dudagoisclaret08 <dudagois86@gmail.com>
2025-08-27 15:36:14 -0300

    Update funci.py

--- funci.py
+++ funci.py
@@ -27,6 +27,44 @@ for nome, cargo in cursor.fetchall():
 banco.close()
 
 
+#### atvdd 2
+
+import sqlite3
+
+# Conecta no banco escola.db
+conn = sqlite3.connect('escola.db')
+cursor = conn.cursor()
+
+# 1. Listar nome e nota de todos os alunos
+print("Nome e nota de todos os alunos:")
+cursor.execute("SELECT nome, nota FROM alunos")
+for nome, nota in cursor.fetchall():
+    print(f"Nome: {nome}, Nota: {nota}")
+
+print("\n")
+
+2. Selecionar apenas o aluno cujo id = 3
+print("Dados do aluno com id = 3:")
+cursor.execute("SELECT * FROM alunos WHERE id = 3")
+aluno = cursor.fetchone()
+if aluno:
+    print(aluno)
+else:
+    print("Aluno com id = 3 não encontrado.")
+
+print("\n")
+
+# 3. Exibir alunos da turma 'B' com nota >= 7
+print("Alunos da turma 'B' com nota maior ou igual a 7:")
+cursor.execute("SELECT * FROM alunos WHERE turma = 'B' AND nota >= 7")
+for aluno in cursor.fetchall():
+    print(aluno)
+
+
+conn.close()
+
+
+
 
 
 
